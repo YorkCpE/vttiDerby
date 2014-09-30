18206cf51fc8b29e7e905f1d1d0eff797503faff
Jason Forsyth <Jason Forsyth@netlenovo>
2014-09-30 17:17:44 -0400

    Debouncing pins.

--- Arduino Source/ScienceFair/ScienceFair.ino
+++ Arduino Source/ScienceFair/ScienceFair.ino
@@ -3,7 +3,7 @@
 
 
 //pins for control buttons
-const int LaneViolationPing=2; 
+const int LaneViolationPin=2; 
 const int CollisionWarningPin=3;
 const int LapPin=4;
 const int SystemCheckPin=5;
@@ -21,74 +21,148 @@ const byte LAP_STARTSTOP=0xC;
 const byte HEARTBEAT=0xD;
 const byte SYSTEM_CHECK=0xE;
 
-/*
-This example is for Series 2 XBee
- Sends a ZB TX request with the value of analogRead(pin5) and checks the status response for success
-*/
 
-// create the XBee object
+const byte commandBytes[]={
+  LANE_VIOLATION,COLLISION_WARNING,LAP_STARTSTOP,SYSTEM_CHECK};
+const byte buttonPins[]={
+  LaneViolationPin,CollisionWarningPin,LapPin,SystemCheckPin};
+
+int buttonStates[]={
+  0,0,0,0};
+int lastDebounceTimes[]={
+  0,0,0,0};
+
+const byte myVehicle=TriangleRed;
+
 XBee xbee = XBee();
 
-uint8_t payload[] = { 0, 0 };
+// allocate two bytes for to hold a 10-bit analog reading
+uint8_t payload[] = { 
+  0, 0, 0, 0 };
 
-// SH + SL Address of receiving XBee
-XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x403e0f30);
-ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
-ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+// with Series 1 you can use either 16-bit or 64-bit addressing
 
+TxStatusResponse txStatus = TxStatusResponse();
 
 void setup()
 {
-  Serial.begin(9600);
-
-  xbee.setSerial(Serial);
+  Serial.begin(57600);
 
   //setup button pins
-  pinMode(LaneViolationPing,INPUT);
+  pinMode(LaneViolationPin,INPUT);
   pinMode(CollisionWarningPin,INPUT);
   pinMode(LapPin,INPUT);
   pinMode(SystemCheckPin,INPUT);
 }
 
-void loop()
+boolean packetSent=false;
+void sendCommandByte(byte commandByte, byte vehicle)
 {
-//    xbee.send(zbTx);
+  byte argBytes[]={
+    0x0,0x0    };
 
-  // after sending a tx request, we expect a status response
-  // wait up to half second for the status response
-  if (xbee.readPacket(500)) 
-  {
-    // got a response!
+  byte checkSum=(byte) (commandByte^argBytes[0]^argBytes[1]^0xff);
 
-    // should be a znet tx status            	
-    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) 
+  byte payload[]={
+    commandByte,argBytes[0],argBytes[1],checkSum    };
+
+  Tx16Request tx = Tx16Request(vehicle, payload, sizeof(payload));  
+
+  xbee.send(tx);
+
+  packetSent=true;
+}
+
+const long debounceDelay=50;
+boolean debouncePin(int pin)
+{
+  int* lastButtonState=&buttonStates[pin%4];
+  int* lastDebounceTime=&lastDebounceTimes[pin%4];
+  int buttonState=0;
+
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(pin);
+
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to HIGH),  and you've waited 
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != *lastButtonState) {
+    // reset the debouncing timer
+    *lastDebounceTime = millis();
+  } 
+
+  if ((millis() - *lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) 
     {
-      xbee.getResponse().getZBTxStatusResponse(txStatus);
+      buttonState = reading;
 
-      // get the delivery status, the fifth byte
-      if (txStatus.getDeliveryStatus() == SUCCESS) 
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) 
       {
-        // success.  time to celebrate
-      } 
-      else 
-      {
-        // the remote XBee did not receive our packet. is it powered on?
+        return true;
       }
     }
-  } 
-  else if (xbee.getResponse().isError()) 
+  }
+
+  return false;
+}
+void loop()
+{
+
+  for(int i=0;i<4;i++)
   {
-    //nss.print("Error reading packet.  Error code: ");  
-    //nss.println(xbee.getResponse().getErrorCode());
-  } 
-  else 
+    if(debouncePin(buttonPins[i]))
+    {
+      sendCommandByte(commandBytes[i],myVehicle); 
+    }
+  }
+
+  // after sending a tx request, we expect a status response
+  // wait up to 5 seconds for the status response
+  if(packetSent)
   {
-    // local XBee did not provide a timely TX Status Response -- should not happen
-    //flashLed(errorLed, 2, 50);
+    packetSent=false;
+    if (xbee.readPacket(5000)) 
+    {
+      // got a response!
+      // should be a znet tx status            	
+      if (xbee.getResponse().getApiId() == TX_STATUS_RESPONSE) 
+      {
+        xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+        // get the delivery status, the fifth byte
+        if (txStatus.getStatus() == SUCCESS) 
+        {
+          // success.  time to celebrate
+        } 
+        else 
+        {
+          // the remote XBee did not receive our packet. is it powered on?
+        }
+      }      
+    } 
+    else if (xbee.getResponse().isError()) 
+    {
+
+    } 
+    else 
+    {
+      // local XBee did not provide a timely TX Status Response.  Radio is not configured properly or connected
+    }
   }
 
   delay(10);
 }
+
+
+
+
 
 
 
